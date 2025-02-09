6b572cb18635dcd272e014fe4ca63499a9450ecf
ambsprad <ambsprad@gmail.com>
2025-02-09 07:24:11 -0500

    updated fetch url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // we are going ot make an event listener.. 
 // it will will trigger when the DOM is loaded (aka - upon visting the webpage)
 addEventListener("DOMContentLoaded", async function () {
-    const response = await fetch("http://localhost:3000/api/songs")
+    const response = await fetch("https://sly-alive-visor.glitch.me")
     const songs = await response.json()
 
     let html = ""
