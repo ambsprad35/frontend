468622735c8f5a6fe6c9021840525d6155cc6645
ambsprad35 <aspradley2@ivytech.edu>
2025-03-02 17:06:10 -0500

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -8,7 +8,7 @@ class Auth{
 
     validateAuth(auth){
         if(auth != 1){
-            window.location.replace("./login.html")
+            window.location.replace("/frontend/login.html")
         }
         else{
             document.querySelector("body").style.display = "block"
@@ -20,6 +20,6 @@ class Auth{
         localStorage.removeItem("token")
         localStorage.removeItem("uname")
 
-        window.location.replace("./login.html")
+        window.location.replace("/frontend/login.html")
     }
 }
